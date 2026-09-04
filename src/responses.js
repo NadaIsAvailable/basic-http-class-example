4f8d87cebabaf37cc058bb0aa8a5be915998ac0e
Kevin Zheng (RIT Student) <kz3175@rit.edu>
2026-09-04 14:45:02 -0400

    refactored responses and server file

--- src/responses.js
+++ src/responses.js
@@ -1,30 +1,34 @@
+const { resolve } = require('dns');
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const client2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 
-const sendPage = (request, response, page) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(page);
+const style = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+// const sendPage = (request, response, page) => {
+//     response.writeHead(200, { 'Content-Type': 'text/html' });
+//     response.write(page);
+//     response.end();
+// };
+
+const serveFile = (request, response, content, mimeType) => {
+    response.writeHead(200, { 'Content-Type': mimeType });
+    response.write(content);
     response.end();
 };
 
-const getMessage = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
-    response.end();
-};
+const getMessage = (request, response) => serveFile(request, response, 'Hello World', 'text/plain');
 
-const getIndex = (request, response) => {
-    sendPage(request, response, index);
-};
+const getIndex = (request, response) => serveFile(request, response, index, 'text/html');
 
-const getClient2 = (request, response) => {
-    sendPage(request, response, client2);
-};
+const getClient2 = (request, response) => serveFile(request, response, client2, 'text/html');
+
+const getCSS = (request, response) => serveFile(request, response, style, 'text/css');
 
 module.exports = {
     getIndex,
     getClient2,
     getMessage,
+    getCSS,
 };
